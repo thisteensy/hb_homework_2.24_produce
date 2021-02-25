f2a245c71f9c9b1525f03ca0deb504b7ec43c31e
Tina Haiser <tinahaiser@gmail.com>
2021-02-25 02:06:13 +0000

    sorry, I thought I had been successfully tracking changes with commits, but evidently I was not. I created the function, and called it for all three days. I still want to iterate through the text files if I can instead of opening each one separately

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,27 @@
+def report(the_file):
+   for line in the_file:
+       line = line.rstrip()
+       words = line.split('|')
+       
+       melon = words[0]
+       count = words[1]
+       amount = words[2]
+       
+       print("Delivered {} {}s for total of ${}".format(
+        count, melon, amount)) 
 print("Day 1")
+
 the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
+report(the_file)
 the_file.close()
-
 
 print("Day 2")
 the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
+report(the_file)
 the_file.close()
 
 
 print("Day 3")
 the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
+report(the_file)
 the_file.close()
